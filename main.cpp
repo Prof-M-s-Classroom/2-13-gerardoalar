7101bf2961f9d0486cba0d6ff8f24b15ab8e4c1c
gerardoalar <gerardo.alarcon2000@gmail.com>
2025-02-13 20:14:43 -0800

    all methods implemented, although an error with dellast method

--- main.cpp
+++ main.cpp
@@ -34,6 +34,24 @@ public:
         this->length = 1;
         this->head = new Node<T>(value);
     }
+    ~LinkedList() { //destructor that was written in class
+        Node<T> *current = this->head;
+        while (head != NULL) {
+            head = head->next;
+            delete current;
+            current = head;
+        }
+    }
+    Node<T>* getAt(int index) { //method written in class
+        if (index < 0 || index >= length) {
+            return nullptr;
+        }
+        Node<T>* temp = head;
+        for (int i=0; i<index; i++) {
+            temp = temp->next;
+        }
+        return temp;
+    }
     void add(T *value) {
         Node<T> *newNode = new Node<T>(value);
         Node<T> *temp = head;
@@ -59,23 +77,70 @@ public:
     }
 
     void dellast() {
-        Node<T> *temp = head;
-        while (temp->next!=NULL)
-            temp = temp->next;
+        Node<T>* temp = head;
+        while (temp->next != NULL) {
+            temp=temp->next;
+        }
         delete temp;
         length--;
     }
 
     void deleteNode(int index) {
-       //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
+        int llLength = this->length;
+        if (index > llLength - 1) { //edge case if the user wants to delete the last node, method we already wrote
+            dellast();
+            return;
+        }
+        if (index == 0) { //edge case for if the user wants to delete the head, also a method already written
+            delfirst();
+            return;
+        }
+        Node<T> *temp = head;
+        Node<T> *temp2 = head;
+        int count = 0;
+        while (count < index) { //temp is one node ahead of temp2
+            temp = temp->next;
+            count++;
+            if (count < index) {
+                temp2 = temp2->next;
+            }
+        }
+        temp2->next = temp->next; //previous node now points one more node ahead because the next node will be deleted
+        delete temp;
+        length--;
     }
 
-   void insert(int index, T *value) {
-        //TODO:Write a function to insert a new node at a give index. Reuse the pre-written functions for edge cases. Account for missing index
+   void insert(int index, T *value) { //method written in class
+        if (index < 0 || index >= length) {
+            cout<<"Index is invalid"<<endl;
+            return;
+        }
+        if (index == 0) {
+            addhead(value);
+        }
+        if(index == length){
+            add(value);
+        }
+        else {
+            Node<T>* newNode = new Node<T>(value);
+            Node<T> *temp = getAt(index);
+            newNode->next = temp->next;
+            temp->next=newNode;
+            length++;
+        }
     }
 
    void reverselist(){
-        //TODO:Write a function to reverse the list using the logic from the slide.
+        Node<T> *prev, curr, next;
+        prev = NULL;
+        curr = head;
+        while (curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
     void print() {
